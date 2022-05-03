e410c09add75b3a4868eb19f4242eb2e3701244e
FatahChan <ahmadfathallah89@gmail.com>
2022-05-03 18:30:54 +0200

    Solved Hierarchy

--- Sheet_A/Hierarchy.cpp
+++ Sheet_A/Hierarchy.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 
+
 using namespace std;
 
 inline void FastIO() {
@@ -8,7 +9,92 @@ inline void FastIO() {
     cout.tie(nullptr);
 }
 
+
+void DFS(int src,vector<set<int>> &undirectedAdjList, vector<bool> &visited, set<int> &set) {
+    visited[src] = true;
+    set.insert(src);
+    for (int i : undirectedAdjList[src]) {
+        if(!visited[i]) DFS(i, undirectedAdjList, visited, set);
+    }
+}
+
+vector<set<int>> connected_directed_graph(vector<set<int>> &undirectedAdjList, int n){
+    vector<set<int>> connected_graphs;
+    vector<bool> visited(n+1, false);
+    for (int i = 1; i <= n; ++i) {
+        if(!visited[i]){
+            set<int> s;
+            DFS(i, undirectedAdjList, visited, s);
+            connected_graphs.push_back(s);
+        }
+    }
+    return connected_graphs;
+}
+
+
+void topological_sort(int v, stack<int> &stack, unordered_map<int, bool> &visited, vector<set<int>> &undirected_graph){
+    visited[v] = true;
+    for (int i : undirected_graph[v]) {
+        if(!visited[i]) topological_sort(i, stack, visited, undirected_graph);
+    }
+    stack.push(v);
+}
+
+void build_hierarchy(set<int> &s, int &mainBoss, vector<int> &bosses, vector<set<int>> &directed_graph, int k){
+    unordered_map<int, bool> visited;
+    for (int i: s) {
+        visited[i] = false;
+    }
+    stack<int> stack;
+    for (int i: s) {
+        if (!visited[i]) {
+            topological_sort(i, stack, visited, directed_graph);
+        }
+    }
+
+
+
+
+    int lastBoss = stack.top();
+    if(mainBoss == 0){
+        mainBoss = stack.top();
+        bosses[stack.top()] = 0;
+    }else{
+        bosses[stack.top()] = mainBoss;
+    }
+    stack.pop();
+    while (!stack.empty()){
+        bosses[stack.top()] = lastBoss;
+        if(stack.top() <= k ){
+            lastBoss = stack.top();
+        }
+        stack.pop();
+    }
+}
+
 int main() {
     FastIO();
-    cout << "Hello World";
+    int n, k, x, y;
+    cin >> n >> k;
+    vector<set<int>> directed_graph(n+1), undirected_graph(n+1);
+    vector<int> bosses(n+1);
+    int mainBoss = 0;
+    for (int i = 1; i <= k; ++i) {
+        cin >> x;
+        for (int j = 0; j < x; ++j) {
+            cin >> y;
+            directed_graph[i].insert(y);
+            undirected_graph[i].insert(y);
+            undirected_graph[y].insert(i);
+        }
+    }
+    vector<set<int>> CDG =  connected_directed_graph(undirected_graph,n);
+    for (auto s: CDG) {
+        build_hierarchy(s, mainBoss, bosses, directed_graph, k);
+    }
+    for (int i = 1; i <= n; ++i) {
+        cout << bosses[i] << endl;
+    }
+
+
 }
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Storm/CLionProjects/Junior_Training_Sheet
+# Install script for directory: D:/CP/Junior_Training_Sheet
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Users/Storm/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/221.5080.224/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Users/Tempest/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/221.5080.224/bin/mingw/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Storm/CLionProjects/Junior_Training_Sheet/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/CP/Junior_Training_Sheet/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
