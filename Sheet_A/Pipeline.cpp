4d3c0852fb6c97864bac94c6d289ce2de422d712
FatahChan <Ahmadfatllah89@gmail.com>
2022-06-06 04:49:09 +0200

    Refactor pipelines

--- Sheet_A/Pipeline.cpp
+++ Sheet_A/Pipeline.cpp
@@ -8,21 +8,51 @@ inline void FastIO() {
     cout.tie(nullptr);
 }
 
+inline int64_t sum(int64_t k) {
+    return k * (k + 1) / 2;
+}
+
+inline int64_t sum(int64_t l, int64_t r) {
+    return sum(r) - sum(l - 1);
+}
+
+inline int64_t search(int64_t n, int64_t k) {
+    int64_t left = 1, right = k, mid, s;
+    while (left < right) {
+        mid = left + (right - left) / 2;
+        s = sum(mid, k);
+        if (s == n) {
+            return k - mid + 1;
+        } else if (s > n) {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+    return k - left + 2;
+}
+
+
 int main() {
     FastIO();
-    int64_t n, k, m, i, x;
+    int64_t n, k;
     cin >> n >> k;
-    k = min(k, n);
-    m = ((k * k) - k) / 2;
-    m++;
-    i = m - n;
-    if (i < 0) {
-        cout << -1;
-    } else if (i == 0) {
+    int64_t max_outlet = sum(k - 1) + 1;
+
+
+    if (n == 1) {
+        cout << 0;
+    } else if (n <= k) {
+        cout << 1;
+    } else if (n == max_outlet) {
         cout << k - 1;
+    } else if (n > max_outlet) {
+        cout << -1;
     } else {
-        x = int64_t(sqrt(8 * i - 1)) / 2;
-        cout << k - 1 - x;
+        n -= 1;
+        k -= 1;
+        cout << search(n, k);
     }
+
 
 }
